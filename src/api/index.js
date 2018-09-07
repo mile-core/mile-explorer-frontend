519c851cd2abdcf977c9c761b0f1df62b52a6826
Denis Aleshin <denis.aleshin@moex.com>
2018-09-07 18:43:17 +0300

    describe all api methods

--- src/api/index.js
+++ src/api/index.js
@@ -15,28 +15,63 @@ function jsonRpc(method, params = {}, id = uuidv4()) {
 }
 
 export default {
+  // Get all allowed methods
   help() {
     return jsonRpc('help');
   },
+
+  /* Explore blocks */
+
+  // Get chains history state
+  getBlockHistoryState() {
+    return jsonRpc('get-block-history-state');
+  },
+
+  // Get chains history from block first-id with limit of history length
+  getBlockHistory(firstId, limit = 1) {
+    if (limit === 1) return this.getBlock(firstId);
+    return jsonRpc('get-block-history', { 'first-id': firstId, limit });
+  },
+
+  // Get certain block from chain by id.
+  // This is equal get-block-history with limit:1 but more efficient
   getBlock(blockId) {
     return jsonRpc('get-block', { id: blockId });
   },
-  getBlockHistory() {
-    return Promise.resolve({}); // TODO: implement
+
+  // Get wallet block ids for a known *public-key*
+  getWalletHistoryBlocks(publicKey, blockId, limit = 2) {
+    return jsonRpc('get-wallet-history-blocks', {
+      'public-key': publicKey,
+      'block-id': blockId,
+      limit,
+    });
   },
-  getBlockHistoryState() {
-    return Promise.resolve({}); // TODO: implement
+
+  /* Explore wallet and transactions */
+
+  // Get wallet history state for a known *public-key*
+  getWalletHistoryState(publicKey) {
+    return jsonRpc('get-wallet-history-state', {
+      'public-key': publicKey,
+    });
   },
-  getTransactionInfo() {
-    return Promise.resolve({}); // TODO: implement
+
+  // Get wallet transaction history for the known public-key
+  // from first-id with limit of transactions list
+  getWalletHistoryTransactions(publicKey, firstId, limit = 3) {
+    return jsonRpc('get-wallet-history-transactions', {
+      'public-key': publicKey,
+      'first-id': firstId,
+      limit,
+    });
   },
-  getWalletHistoryBlocks() {
-    return Promise.resolve({}); // TODO: implement
-  },
-  getWalletHistoryState() {
-    return Promise.resolve({}); // TODO: implement
-  },
-  getWalletHistoryTransactions() {
-    return Promise.resolve({}); // TODO: implement
+
+  // Get certain transactions for the known wallet with public-key and *id*
+  getTransactionInfo(publicKey, id) {
+    return jsonRpc('get-transaction-info', {
+      'public-key': publicKey,
+      id,
+    });
   },
 };
